66de10506b325aa15ec86293bba5d0812e686da1
Aurelie Lebec <aurelie.lebec@gmail.com>
2018-02-17 20:13:19 -0600

    [added favicon]

--- client/services/soundCloud.js
+++ client/services/soundCloud.js
@@ -1,15 +1,15 @@
 angular.module('podcast')
   .service('SoundCloud', function ($http) {
-    
+
     this.search = function (query, callback) {
       const TOKEN = window.token.TOKEN;
-      //const TOKEN = process.env.TOKEN;
+      // const TOKEN = process.env.TOKEN;
       return $http({
         method: 'GET',
         url: `https://api.soundcloud.com/tracks?client_id=${TOKEN}&q=${query}`,
       })
         .then(results => callback(results.data))
-        .catch(err => console.log(err))
-  }
-  })  
+        .catch(err => console.log(err));
+    };
+  });
 
